006a610e7957264d0301f01811dd1720f7ba3549
nursetta <n_ursetta@yahoo.com>
2016-07-26 15:11:30 -0600

    created show route

--- records/app/controllers/records_controller.rb
+++ records/app/controllers/records_controller.rb
@@ -1,6 +1,11 @@
 class RecordsController < ApplicationController
 	def index
 		@records = Record.all
-		# render :index # optional
+		render :index # optional
+	end
+
+	def show
+		@record = Record.find(params[:id])
+		render :show
 	end
 end
--- records/config/routes.rb
+++ records/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-resources :records, only: [:index]
+	resources :records, only: [:index]
+	resources :records, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
